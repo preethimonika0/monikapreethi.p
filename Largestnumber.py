2db99a8bc38bf6d7d22c33bdd6718f2010376fbe
monikapreethi.p <42573405+preethimonika0@users.noreply.github.com>
2018-09-06 12:17:39 +0530

    Update Largestnumber.py

--- Largestnumber.py
+++ Largestnumber.py
@@ -1,10 +1,10 @@
 n1=int(raw_input())
 n2=int(raw_input())
 n3=int(raw_input())
-if ((n1>n2) and (n1>n3)):
+if (n1>=n2) and (n1>n3):
 	largest=n1
-elif ((n2>n1) and (n2>n3)):
+elif (n2>=n1) and (n2>=n3):
 	largest=n2
 else:
 	largest=n3
-print "
+print("the largest number between",n1,",",n2,",",n3,"is",largest)

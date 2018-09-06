46eacaf70219cef8e6b040b2388a7feb7150dbf7
monikapreethi.p <42573405+preethimonika0@users.noreply.github.com>
2018-09-06 12:08:08 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,11 @@
+year=1998
+if (year%4)==0:
+	if(year%100)==0:
+		print("{0} is a leap year".format(year))
+		else:
+			print("{0} is not a leap year".format(year))
+		else:
+			print("{0} is a leap year".format(year))
+		else:
+			print("{0} is not a leap year".format(year))
+			
